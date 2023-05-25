6f4642200802dfab636e357bc1f1ef7919a017fc
Jared Skinner <jaredrskinner@gmail.com>
2023-05-24 23:48:07 -0600

    Fix windows

--- sdsetup.bat
+++ sdsetup.bat
@@ -1,14 +1,13 @@
 @echo off
-
 set /p "id0=ID0: "
 set /p "id1=ID1: "
 
-echo Downloading SafeB9SInstaller 0.0.7, boot9strap 1.4, and luma 3ds 11.0... 
+echo Downloading SafeB9SInstaller, boot9strap, and luma 3ds... 
 echo.
 
-curl -s https://api.github.com/repos/d0k3/SafeB9SInstaller/releases/latest | jq -r ".assets[0] | .browser_download_url" | wget -qO ./SafeB9SInstaller.zip -i -
-curl -s https://api.github.com/repos/SciresM/boot9strap/releases/latest | jq -r ".assets[0] | .browser_download_url" | wget -qO ./boot9strap.zip -i -
-curl -s https://api.github.com/repos/LumaTeam/Luma3DS/releases/latest | jq -r ".assets[0] | .browser_download_url" | wget -qO ./Luma3DS.zip -i -
+for /f "tokens=1,* delims=:" %%A in ('curl -ks https://api.github.com/repos/d0k3/SafeB9SInstaller/releases/latest ^| find "browser_download_url"') do (curl -kL -o SafeB9SInstaller.zip  %%B)
+for /f "tokens=1,* delims=:" %%A in ('curl -ks https://api.github.com/repos/SciresM/boot9strap/releases/releases/latest ^| find "browser_download_url"') do (curl -kL -o boot9strap.zip  %%B)
+for /f "tokens=1,* delims=:" %%A in ('curl -ks https://api.github.com/repos/LumaTeam/Luma3DS/releases/latest ^| find "browser_download_url"') do (curl -kL -o Luma3DS.zip  %%B)
 curl -LJO https://github.com/zoogie/unSAFE_MODE/releases/download/v1.3/usm.bin
 
 cls
@@ -39,6 +38,7 @@ mkdir "%sd%\Nintendo 3DS\%id0%"
 mkdir "%sd%\Nintendo 3DS\%id0%\%id1%"
 mkdir "%sd%\Nintendo 3DS\%id0%\%id1%\Nintendo DSiWare"
 move F00D43D5.bin "%CD%\%sd%\Nintendo 3DS\%id0%\%id1%\Nintendo DSiWare\"
+move bb3.bin %CD%\%sd%
 move usm.bin %CD%\%sd%
 
 
@@ -47,22 +47,26 @@ echo.
 
 
 echo Downloading FBI (cia and 3dsx), Anemone, Checkpoint, Universal Updater, GodMode9, and the Homebrew Launcher Loader.
-curl -s https://api.github.com/repos/astronautlevel2/Anemone3DS/releases/latest | jq -r ".assets[0] | .browser_download_url" | wget -qO ./Anemone3DS.cia -i -
-curl -s https://api.github.com/repos/FlagBrew/Checkpoint/releases/latest | jq -r ".assets[0] | .browser_download_url" | wget -qO ./Checkpoint.cia -i -
-curl -s https://api.github.com/repos/PabloMK7/homebrew_launcher_dummy/releases/latest | jq -r ".assets[0] | .browser_download_url" | wget -qO ./Homebrew_Launcher.cia -i -
-curl -s https://api.github.com/repos/Universal-Team/Universal-Updater/releases/latest | jq -r ".assets[0] | .browser_download_url" | wget -qO ./Universal-Updater.cia -i -
-curl -s https://api.github.com/repos/Steveice10/FBI/releases/latest | jq -r ".assets[0] | .browser_download_url" | wget -qO ./FBI.3dsx -i -
-curl -s https://api.github.com/repos/Steveice10/FBI/releases/latest | jq -r ".assets[1] | .browser_download_url" | wget -qO ./FBI.cia -i -
-curl -s https://api.github.com/repos/d0k3/GodMode9/releases/latest | jq -r ".assets[0] | .browser_download_url" | wget -qO ./GodMode9.zip -i -
-
+for /f "tokens=1,* delims=:" %%A in ('curl -ks https://api.github.com/repos/astronautlevel2/Anemone3DS/releases/latest ^| find "browser_download_url"') do (curl -kL -o Anemone3DS.cia  %%B)
+for /f "tokens=1,* delims=:" %%A in ('curl -ks https://api.github.com/repos/FlagBrew/Checkpoint/releases/latest ^| find "browser_download_url"') do (curl -kL -o Checkpoint.cia  %%B)
+for /f "tokens=1,* delims=:" %%A in ('curl -ks https://api.github.com/repos/PabloMK7/homebrew_launcher_dummy/releases/latest ^| find "browser_download_url"') do (curl -kL -o Homebrew_Launcher.cia  %%B)
+for /f "tokens=1,* delims=:" %%A in ('curl -ks https://api.github.com/repos/Universal-Team/Universal-Updater/releases/latest ^| find "browser_download_url"') do (curl -kL -o Universal-Updater.cia  %%B)
+for /f "tokens=1,* delims=:" %%A in ('curl -ks https://api.github.com/repos/Steveice10/FBI/releases/latest ^| find "browser_download_url"') do (curl -kL -o FBI.zip  %%B)
+tar -xvf FBI.zip 3ds-arm/3ds/FBI/FBI.3dsx
+tar -xvf FBI.zip 3ds-arm/FBI.cia -C %CD%
+del FBI.zip
+for /f "tokens=1,* delims=:" %%A in ('curl -ks https://api.github.com/repos/d0k3/GodMode9/releases/latest ^| find "browser_download_url"') do (curl -kL -o GodMode9.zip  %%B)
 echo Moving Shtuffs...
 echo.
 
 mkdir %CD%\%sd%\cias
 move *.cia %CD%\%sd%\cias\
+move %CD%\%sd%\3ds-arm\FBI.cia %CD%\%sd%\3ds\
 
 mkdir %CD%\%sd%\3ds
-move *.3ds  %CD%\%sd%\3ds\
+move %CD%\3ds-arm\3ds\FBI\FBI.3dsx  %CD%\%sd%\3ds\
+
+rmdir /S %CD%\3ds-arm
 
 mkdir %CD%\%sd%\luma
 mkdir %CD%\%sd%\luma\payloads
